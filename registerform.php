56bb6d69f474207ff72b0254a35370373818b3e4
Vy Tran <vytran1805@gmail.com>
2022-12-09 11:30:37 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   cart.js "deleted all the cart function"
    	modified:   products.php "deleted bootstrap css"
    	modified:   registerform.php "deleted bootstrap css"

--- registerform.php
+++ registerform.php
@@ -28,7 +28,7 @@ Brief description of Assignment 2: The purpose of this webpage is to let the use
                     <a href="products.php">SHOP</a>
                     <a href="contactUs.php">CONTACT US</a>             
                     <a href="myaccount.php"><b>MY ACCOUNT</b></a>
-                    <a href="" >
+                    <a href="cart.php" >
                         <span id="cart-icon">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
                                 <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
